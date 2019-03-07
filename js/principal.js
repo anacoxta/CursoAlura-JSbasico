53f0296ce4e8b24451ab3f84223752bf423f4821
Ana Costa Eduardo <ana.costa.eduardo@gmail.com>
2019-03-07 19:17:59 -0300

    Atividade 08 completa (marcando o fim da Aula 1)

---
+++ js/principal.js
@@ -0,0 +1,2 @@
+var titulo = document.querySelector(".titulo"); //> busca o elemento com classe 'titulo' e atribui à var
+titulo.textContent = "Aparecida Nutricionista"; //replaces the content inside the h1 tag
