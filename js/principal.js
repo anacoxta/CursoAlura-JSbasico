5f87b1255c4f9d95660bfb3adc05535efca8926b
Ana Costa Eduardo <ana.costa.eduardo@gmail.com>
2019-03-08 19:09:52 -0300

    Cálculo IMC todos os pacientes + validação melhorzinha <- aula03 ativ01

--- js/principal.js
+++ js/principal.js
@@ -1,33 +1,38 @@
 var titulo = document.querySelector(".titulo"); //> busca o elemento com classe 'titulo' e atribui à var
 titulo.textContent = "Aparecida Nutricionista"; //replaces the content inside the h1 tag
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
   
-var peso = paciente.querySelector(".info-peso").textContent;
-var altura = paciente.querySelector(".info-altura").textContent;
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
 
-var tdImc = paciente.querySelector(".info-imc");
+    var altura = tdAltura.textContent;
+    var peso = tdPeso.textContent;
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
-if (peso <= 0 || peso >= 600) {
-    console.log("Peso inválido");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido";
-}
+    if (peso <= 0 || peso >= 600) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdPeso.textContent = "Peso inválido";
+    }
 
-if (altura <= 0 || altura >= 3) {
-    console.log("Altura inválida");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida";
-}
+    if (altura <= 0 || altura >= 3) {
+        console.log("Altura inválida");
+        alturaEhValida = false;
+        tdAltura.textContent = "Altura inválida";
+    }
 
-//Existe um claro problema de verificação nesta solução, oferecida pelo curso (pelo menos até este ponto).
-//Se ambos peso e altura forem inválidos, a verificação no console.log indicará os dois erros...
-//...mas o preenchimento da tabela só mostrará "Altura inválida"
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura **2); //**2 = ao quadrado (es6)
+        tdImc.textContent = imc.toFixed(2);
+    } else {
+        tdImc.textContent = "Corrija o(s) dado(s) inválido(s)."
+    }
 
-if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura **2); //**2 = ao quadrado (es6)
-    tdImc.textContent = imc;
 }
