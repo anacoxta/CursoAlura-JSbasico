0f1b0a5304dcb42604fd1837a4341c28e94e765a
Ana Costa Eduardo <ana.costa.eduardo@gmail.com>
2019-03-10 22:36:09 -0300

    Formulario p add novos pacientes <- aula4 completa

--- js/principal.js
+++ js/principal.js
@@ -40,3 +40,51 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 
 }
+
+/* ====== MODELO DE FUNÇÃO DE EVENTO ========
+
+var botao = document.querySelector("#botao")
+
+1) Declarando função à parte:
+    funcaoSeparada() {
+        console.log("mensagem");
+    };
+    botao.addEventListener("click", funcaoSeparada);
+
+2) Usando função anônima:
+    botao.addEventListener("click", function(){
+        console.log("mensagem");
+    });
+
+============================================ */
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(cliqueBotao) {
+    cliqueBotao.preventDefault();
+    //variaveis abaixo armazenam os dados de cada campo
+    var form = document.querySelector("#form-adiciona");
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+    //criando novos elementos (linha e células de tabela) para receber os dados inputados
+    var pacienteTr = document.createElement("tr");
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+    //passando os valores dos inputs pras celulas
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    //adicionando cada célula TD dentro da linha TR
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    //selecionar a tabela pelo ID e adicionar a linha TR como filha
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+});
